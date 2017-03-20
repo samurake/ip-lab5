a2a7c235360f7a300454f8de4bb35324c5a96133
mpostolache96 <postolachealexm@gmail.com>
2017-03-20 15:25:20 +0200

    Reverse Engineering sources

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,25 +1,45 @@
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/20/2017 3:05:19 PM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Restaurant.java
+
+import java.io.PrintStream;
+import java.util.Iterator;
 import java.util.Vector;
 
-public class Restaurant {
+public class Restaurant
+{
 
-    public Vector<Table> tables;
-
-    public Restaurant() {
-
-        tables = new Vector<>();
+    public Restaurant()
+    {
+        tables = new Vector();
     }
 
-    public void makeReservation(int seatsNumber) {
+    public void makeReservation(int seatsNumber)
+    {
         tables.add(new Table(seatsNumber));
     }
 
-    @Override
-    public String toString() {
-    StringBuilder returnString = new StringBuilder();
+    public String toString()
+    {
+        StringBuilder returnString = new StringBuilder();
+        Table table;
+        for(Iterator iterator = tables.iterator(); iterator.hasNext(); returnString.append(table))
+            table = (Table)iterator.next();
 
-        for (Table table: tables) {
-            returnString.append(table);
-        }
         return returnString.toString();
     }
+
+    public static void main(String args[])
+    {
+        Restaurant rest = new Restaurant();
+        rest.makeReservation(5);
+        rest.makeReservation(90);
+        rest.makeReservation(1);
+        rest.makeReservation(3);
+        rest.makeReservation(2);
+        System.out.print(rest);
+    }
+
+    public Vector tables;
 }
--- src/Table.java
+++ src/Table.java
@@ -1,22 +1,28 @@
-public class Table {
+// Decompiled by DJ v3.10.10.93 Copyright 2007 Atanas Neshkov  Date: 3/20/2017 3:06:00 PM
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+// Source File Name:   Table.java
 
-    public int seats;
 
-    public boolean reserved;
+public class Table
+{
 
-    public Table(int seats) {
-        if (seats > 0) {
+    public Table(int seats)
+    {
+        if(seats > 0)
             this.seats = seats;
-        }
     }
 
-    public void setReserved() {
+    public void setReserved()
+    {
         reserved = true;
     }
 
-    @Override
-    public String toString() {
-        return "Table with " + seats + " seats \n";
+    public String toString()
+    {
+        return (new StringBuilder()).append("Table with ").append(seats).append(" seats \n").toString();
     }
 
+    public int seats;
+    public boolean reserved;
 }
